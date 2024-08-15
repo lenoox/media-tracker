eb3d2bbd26dacbe929fa852887ba6a9a113ff223
lenoox <62141973+lenoox@users.noreply.github.com>
2024-08-15 18:00:28 +0200

    add lodash (#8)

--- src/app/components/menu/menu.test.tsx
+++ src/app/components/menu/menu.test.tsx
@@ -2,25 +2,46 @@ import MockAdapter from "axios-mock-adapter";
 import Menu from "./menu";
 import {screen} from "@testing-library/react";
 import {categoriesData} from "../../services/media-data";
-import axios from "axios";
 import {initUseTranslationForTest, renderComponent,} from "../../utils/utils-for-test";
 import {api} from "../../services/api.service";
+import {act} from "react-dom/test-utils";
 
-export const axiosApi = axios.create();
-const mock = new MockAdapter(axiosApi);
 
 jest.mock("react-i18next", () => ({
   useTranslation: jest.fn(),
 }));
 
 describe("sidebar", () => {
-  beforeEach(async () => {
-    const { translation } = initUseTranslationForTest();
-    mock
-      .onGet(`${api.getApiUrl()}/genre/movie/list`)
-      .reply(200, categoriesData);
-    renderComponent(<Menu />);
+  let mock: MockAdapter;
+
+  beforeAll(() => {
+    // Initialize the mock adapter for Axios
+    mock = new MockAdapter(api.axiosApi);
   });
+
+  beforeEach(async() => {
+    // Set up the GET request to return mock categories data
+    act(() => {
+      mock.onGet(`${api.getApiUrl()}/genre/movie/list`).reply(200, categoriesData);
+    });
+
+    const { translation } = initUseTranslationForTest();
+
+    await act(async () => {
+      renderComponent(<Menu />);
+    });
+  });
+
+  afterEach(() => {
+    // Reset the mock between tests
+    mock.reset();
+  });
+
+  afterAll(() => {
+    // Restore the original Axios instance
+    mock.restore();
+  });
+
 
   it("should render categories", async () => {
     const parent = await screen.findByTestId("categories");
@@ -29,6 +50,7 @@ describe("sidebar", () => {
     expect(child[0]).toBeInTheDocument();
   });
   it("should render 19 categories", async () => {
+
     const parent = await screen.findByTestId("categories");
     const child = await screen.findAllByTestId("category");
     expect(parent).toContainElement(child[0]);
