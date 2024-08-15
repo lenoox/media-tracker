7dd824af5bc7c13b8c6d279019a10e658e3bbef5
lenoox <62141973+lenoox@users.noreply.github.com>
2024-08-15 17:59:57 +0200

    add lodash

--- src/app/components/navbar/navbar.test.tsx
+++ src/app/components/navbar/navbar.test.tsx
@@ -3,43 +3,81 @@ import {fireEvent, screen} from "@testing-library/react";
 import * as router from "react-router";
 import Navbar from "./navbar";
 import {initUseTranslationForTest, renderComponent,} from "../../utils/utils-for-test";
+import MockAdapter from "axios-mock-adapter";
+import {api} from "../../services/api.service";
+import {categoriesData} from "../../services/media-data";
+import {act} from "react-dom/test-utils";
 
 jest.mock("react-i18next", () => ({
   useTranslation: jest.fn(),
 }));
 describe("navbar", () => {
   const mockedNavigation = jest.fn();
-  beforeEach(async () => {
-    jest
-      .spyOn(router, "useNavigate")
-      .mockImplementation(() => mockedNavigation);
-    const { translation } = initUseTranslationForTest();
+  let mock: MockAdapter;
+
+  beforeAll(() => {
+    mock = new MockAdapter(api.axiosInterceptor());
   });
 
+  beforeEach(() => {
+    // Set up the GET request to return mock categories data
+    mock.onGet(`${api.getApiUrl()}/genre/movie/list`).reply(200, categoriesData);
+    jest
+        .spyOn(router, "useNavigate")
+        .mockImplementation(() => mockedNavigation);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+
+
   it("should contain text logo", async () => {
-    renderComponent(<Navbar />);
+    const { translation } = initUseTranslationForTest();
+    await act(async () => {
+      renderComponent(<Navbar />);
+    });
     const logoElement = await screen.findByTestId("logo");
     expect(logoElement).toHaveTextContent("MediaTracker");
   });
 
   it("should contain language selector", async () => {
-    renderComponent(<Navbar />);
+    const { translation } = initUseTranslationForTest();
+    await act(async () => {
+      renderComponent(<Navbar />);
+    });
     expect(screen.queryByTestId("languageSelector")).toBeDefined();
   });
 
   it("should contain dark mode switcher", async () => {
-    renderComponent(<Navbar />);
+    const { translation } = initUseTranslationForTest();
+    await act(async () => {
+      renderComponent(<Navbar />);
+    });
     expect(screen.queryByTestId("darkMode")).toBeDefined();
   });
 
   it("should contain search media component", async () => {
-    renderComponent(<Navbar />);
+    const { translation } = initUseTranslationForTest();
+    await act(async () => {
+      renderComponent(<Navbar />);
+    });
     expect(screen.queryByTestId("searchMedia")).toBeDefined();
   });
 
-  it("should navigate to the home page", () => {
-    const { getByText } = renderComponent(<Navbar />);
-    fireEvent.click(getByText(/MediaTracker/i));
+  it("should navigate to the home page", async() => {
+    const { translation } = initUseTranslationForTest();
+    await act(async () => {
+      renderComponent(<Navbar />);
+    });
+
+    const homeLink = screen.getByText(/MediaTracker/i);
+    fireEvent.click(homeLink);
 
     expect(mockedNavigation).toHaveBeenCalled();
     expect(mockedNavigation.mock.calls[0][0]).toEqual("/");
